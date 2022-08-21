73cfc83adefe53238abe2f244a26bfdfd6cb89d5
habeola <habeolar89@gmai.com>
2022-08-21 09:17:42 +0100

    update recent

--- exqship/settings.py
+++ exqship/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # # for heroku
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 
 # for heroku
