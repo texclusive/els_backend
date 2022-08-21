63993a9e4e5992adafe82fdbba5457f7583f319e
habeola <habeolar89@gmai.com>
2022-08-21 11:50:44 +0100

    settings

--- exqship/settings.py
+++ exqship/settings.py
@@ -182,10 +182,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
